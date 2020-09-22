93540cf9e10d057c8768f0032aca8bf418306b64
galaxy <galaxy@galaxys-iMac.local>
2020-09-23 04:09:57 +0800

    Removed firebase config info.
    Removed firebase config info.

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -1,13 +1,13 @@
 export const firebaseconfig = {
 
     firebase : {
-      apiKey: "AIzaSyARu_pm2XCdhL9TM9bLN_yiyq2Q9lAvWL0",
-      authDomain: "simplyapp-500cf.firebaseapp.com",
-      databaseURL: "https://simplyapp-500cf.firebaseio.com",
-      projectId: "simplyapp-500cf",
-      storageBucket: "simplyapp-500cf.appspot.com",
-      messagingSenderId: "924867286071",
-      appId: "1:924867286071:web:892fa7184cd8f25a439bb2"
+      apiKey: "apiKey",
+      authDomain: "authDomain",
+      databaseURL: "databaseURL",
+      projectId: "projectId",
+      storageBucket: "storageBucket",
+      messagingSenderId: "messagingSenderId",
+      appId: "appId"
 
       }
 
